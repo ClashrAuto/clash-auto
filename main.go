ea778e937bc654def4a88d7f777c89bc3cd4d94e
Clashr Auto <s@ireoo.com>
2022-05-16 21:23:53 +0800

    update

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"github.com/ClashrAuto/clash-auto/ui/window"
+)
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("Clash Auto")
+	w.Resize(fyne.NewSize(960, 570))
+
+	// tabs := container.NewAppTabs(
+	// 	container.NewTabItem("Status", widget.NewLabel("Hello")),
+	// 	container.NewTabItem("Sub", widget.NewLabel("World!")),
+	// )
+
+	// tabs.Append(container.NewTabItemWithIcon("Home", theme.HomeIcon(), widget.NewLabel("Home tab")))
+
+	// tabs.SetTabLocation(container.TabLocationLeading)
+
+	data := []string{"a", "string", "list"}
+
+	w.SetContent(ui.Desktop(data))
+	w.ShowAndRun()
+}
---
+++ ui/window.go
@@ -0,0 +1,35 @@
+package ui
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+func Desktop(data []string) *fyne.Container {
+
+	list := widget.NewList(
+		func() int {
+			return len(data)
+		},
+		func() fyne.CanvasObject {
+			return widget.NewLabel("Loading...")
+		},
+		func(i widget.ListItemID, o fyne.CanvasObject) {
+			o.(*widget.Label).SetText(data[i])
+		},
+	)
+
+	out := container.NewVBox(container.NewAppTabs(
+		container.NewTabItem("Status", container.NewVBox(
+			list,
+		)),
+		container.NewTabItem("Sub", widget.NewLabel("World!")),
+	))
+	// in := widget.NewEntry()
+
+	// in.OnChanged = func(content string) {
+	// 	out.SetText("Hello " + content + "!")
+	// }
+	return out
+}
